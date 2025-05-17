35e9501295ac6fa61cdc591dae6d3ca86740db53
Jeremy Lainé <jeremy.laine@m4x.org>
2025-05-17 18:06:49 +0200

    Remove patch for winpthreads
    Switch Windows build to actual msys2 releases instead of whatever is
    pre-installed on the runner. This means we no longer need to hack
    winpthreads:
    
    - The current release is 2025-02-21 and does not have the issue.
    - The next release will not have the issue either.

--- scripts/build-ffmpeg.py
+++ scripts/build-ffmpeg.py
@@ -336,10 +336,6 @@ def main():
     builder = Builder(dest_dir=dest_dir)
     builder.create_directories()
 
-    # Fix winpthreads breakage until the fix reaches msys2 repos.
-    if plat == "Windows":
-        run(["patch", "-d", "C:/msys64/mingw64", "-i", os.path.join(builder.patch_dir, "winpthreads.patch"), "-p3"])
-
     # install packages
     available_tools = set()
     if plat == "Windows":
