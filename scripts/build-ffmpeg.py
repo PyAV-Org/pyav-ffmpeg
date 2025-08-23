9ee231f1dc4554bf97087fb954a41eed3a3e00f5
WyattBlue <wyattblue@auto-editor.com>
2025-08-22 23:47:46 -0400

    ffmpeg 8.0

--- scripts/build-ffmpeg.py
+++ scripts/build-ffmpeg.py
@@ -236,8 +236,8 @@ nvheaders_package = Package(
 
 ffmpeg_package = Package(
     name="ffmpeg",
-    source_url="https://ffmpeg.org/releases/ffmpeg-7.1.1.tar.xz",
-    sha256="733984395e0dbbe5c046abda2dc49a5544e7e0e1e2366bba849222ae9e3a03b1",
+    source_url="https://ffmpeg.org/releases/ffmpeg-8.0.tar.xz",
+    sha256="b2751fccb6cc4c77708113cd78b561059b6fa904b24162fa0be2d60273d27b8e",
     build_arguments=[],
     build_parallel=plat != "Windows",
 )
