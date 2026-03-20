1e42248dfc8c07084f6424d69891bc4d5a84f434
WyattBlue <wyattblue@auto-editor.com>
2026-03-20 00:23:18 -0400

    Build FFmpeg 8.1

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ import sys
 if sys.platform == "win32":
     include_dirs = ["C:\\cibw\\vendor\\include"]
     library_dirs = ["C:\\cibw\\vendor\\lib"]
+    extra_link_args = []
 else:
     include_dirs = ["/tmp/vendor/include"]
     library_dirs = ["/tmp/vendor/lib"]
+    extra_link_args = ["-headerpad_max_install_names"] if sys.platform == "darwin" else []
 
 setuptools.setup(
     name="dummy",
@@ -17,6 +19,7 @@ setuptools.setup(
             "dummy.binding",
             include_dirs=include_dirs,
             library_dirs=library_dirs,
+            extra_link_args=extra_link_args,
             libraries=[
                 "avformat",
                 "avcodec",
